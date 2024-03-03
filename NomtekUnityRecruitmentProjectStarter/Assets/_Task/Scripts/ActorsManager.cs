ad1343aeca76ea7c666185b309270def87d266f4
Svhugani <mfnowotarski@gmail.com>
2024-03-03 12:59:19 +0100

    implement actors manager, add logic to sceneActor

--- NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/ActorsManager.cs
+++ NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/ActorsManager.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class ActorsManager : MonoBehaviour, IActorsManager
 {
+    [SerializeField] private GameObject ground;
+    [SerializeField] private LayerMask placementLayerMask;
+
+    private SceneActor _previewActor;
+
     public void CancelActorPreview()
     {
-        throw new System.NotImplementedException();
+        if (_previewActor != null)
+        {
+            Destroy(_previewActor.gameObject);
+            _previewActor = null;
+        }
     }
 
     public void SpawnActor()
     {
-        throw new System.NotImplementedException();
+        if (_previewActor != null)
+        {
+            _previewActor.SetToNormalMode();
+            _previewActor = null;
+        }
     }
 
     public void SpawnActorPreview(SceneActor actor)
     {
-        throw new System.NotImplementedException();
+        _previewActor = Instantiate(actor);
+        _previewActor.Initialize();
+        _previewActor.SetToPreviewMode();
     }
 
     public void UpdatePreviewPosition(Vector2 screenPosition)
     {
-        throw new System.NotImplementedException();
+        if (_previewActor == null) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Camera.main.farClipPlane, placementLayerMask))
+        {
+            _previewActor.PlaceOn(hit.point);
+        }
     }
 }
--- NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/SceneActor.cs
+++ NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/SceneActor.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneActor : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public abstract class SceneActor : MonoBehaviour
 {
-    
+    protected Rigidbody Rigidbody { get; private set; }
+    protected MeshRenderer MeshRenderer { get; private set; }
+
+    public virtual void Initialize()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        MeshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public virtual void SetToPreviewMode()
+    {
+        Rigidbody.isKinematic = true;
+    }
+
+    public virtual void SetToNormalMode()
+    {
+        Rigidbody.isKinematic = false;
+    }
+
+    public void PlaceOn(Vector3 position)
+    {
+        transform.position = position + MeshRenderer.bounds.extents.y * Vector3.up;
+    }
+
+    protected abstract void Animate();
 }
