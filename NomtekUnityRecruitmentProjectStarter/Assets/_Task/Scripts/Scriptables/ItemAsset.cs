3a299b7788d3bcfaaea79eb0ecfc969bcd7212fd
Svhugani <mfnowotarski@gmail.com>
2024-03-03 22:14:25 +0100

    actor prefabsd, item asset

--- NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/Scriptables/ItemAsset.cs
+++ NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/Scriptables/ItemAsset.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ActorAsset", menuName = "ScriptableObjects/ActorAsset", order = 1)]
+[CreateAssetMenu(fileName = "ItemAsset", menuName = "ScriptableObjects/ItemAsset", order = 1)]
 public class ItemAsset : ScriptableObject
 {
     [field: SerializeField] public string Text { get; private set; }
