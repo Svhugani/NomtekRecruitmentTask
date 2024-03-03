7e16fa2be567de61ffc8a88273b9c83abc0cd776
Svhugani <mfnowotarski@gmail.com>
2024-03-03 16:36:25 +0100

    implement input manager

--- NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/InputManager.cs
+++ NomtekUnityRecruitmentProjectStarter/Assets/_Task/Scripts/InputManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour, IInputManager
 {
@@ -10,5 +11,35 @@ public class InputManager : MonoBehaviour, IInputManager
     public event Action OnSecondaryConfirm;
     public event Action OnCancel;
 
+    private Vector3 _currentMousePosition;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnCancel?.Invoke();
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+
+        Vector3 mousePosition = Input.mousePosition;
+
+        if (mousePosition != _currentMousePosition)
+        {
+            _currentMousePosition = mousePosition;
+            OnPointerMove?.Invoke(new Vector2(mousePosition.x, mousePosition.y));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnPrimaryConfirm?.Invoke();
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            OnSecondaryConfirm?.Invoke();
+        }
+
+    }
 }
